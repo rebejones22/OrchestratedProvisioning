63f48ade783683b0d581ac380542f0ee6827eb54
Rebecca <rebejones@hotmail.com>
2019-06-26 19:05:33 -0500

    Add files via upload

--- AzureDeploy/Deploy-AzureResourceGroup.ps1
+++ AzureDeploy/Deploy-AzureResourceGroup.ps1
@@ -1,7 +1,7 @@
 #Requires -Version 3.0
 
 Param(
-    [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
+    [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation = 'Central US',
     [string] $ResourceGroupName = 'TeamsProvisioning',
     [switch] $UploadArtifacts,
     [string] $StorageAccountName,
